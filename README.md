955656378000c099a17aeef05195adaa928e0f8d
lvs <lvs.freelance@gmail.com>
2016-10-07 16:56:38 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# LVS.Training.Android.ParseServerClient
+Basic Android Parse Server client, just connect and add data
+
+==Configuration
+Just add APP_ID, CLIENT_ID and Parse Server URL in Strings.xml
+Exemple :
+
---
+++ app/src/main/java/com/lvsandroid/parseserverclient/MainActivity.java
@@ -0,0 +1,37 @@
+package com.lvsandroid.parseserverclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
+        ParseObject score = new ParseObject("Score");
+        score.put("username","pedro");
+        score.put("score",96);
+        score.saveInBackground(new SaveCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.i("Parse", "Save Succeeded");
+                } else {
+                    Log.e("Parse", "Save Failed");
+                }
+            }
+        });
+    }
+}
